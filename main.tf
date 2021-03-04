fbb78e253840ae65cd8b909f5b4a6923adeb8bdc
Omid Ayoobe <oayoo001@gold.ac.uk>
2021-03-04 13:00:52 +0000

    Adding AWS keys

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 provider "aws" {
   region = var.my_region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
--- variable.tf
+++ variable.tf
@@ -1,3 +1,11 @@
 variable "my_region" {
   default = "eu-west-1"
 }
+
+variable "access_key" {
+  default = "access_key"
+}
+
+variable "secret_key" {
+  default = "secret_key"
+}
