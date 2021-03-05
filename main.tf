b18349e6c4998e7d1a3cefc149d4603e78e83883
Omid Ayoobe <oayoo001@gold.ac.uk>
2021-03-05 13:10:20 +0000

    adding ec2 to VPC

--- main.tf
+++ main.tf
@@ -7,16 +7,22 @@ provider "aws" {
 resource "aws_instance" "instance_1" {
   ami = var.ami
   instance_type = "t2.micro"
-
+  key_name = "and_project_instance_1"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.public-subnet-1.id
+  vpc_security_group_ids = aws_security_group.app_security_group.id
   tags = {
-    Name = "instance_2"
+    Name = "instance_1"
   }
 }
 
 resource "aws_instance" "instance_2" {
   ami = var.ami
   instance_type = "t2.micro"
-
+  key_name = "and_project_instance_2"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.public-subnet-2.id
+  vpc_security_group_ids = aws_security_group.app_security_group.id
   tags = {
     Name = "instance_2"
   }
