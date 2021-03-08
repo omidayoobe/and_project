9f45dfc4f384dcd9b57613aca92101c08879ecf6
Omid Ayoobe <oayoo001@gold.ac.uk>
2021-03-08 14:21:01 +0000

    minor changes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "instance_1" {
   ami = var.ami
   instance_type = "t2.micro"
-  key_name = "and_project_instance_1"
+  key_name = "and_key_pair"
   associate_public_ip_address = true
   subnet_id = aws_subnet.public-subnet-1.id
   vpc_security_group_ids = ["${aws_security_group.app_security_group.id}"]
@@ -19,7 +19,7 @@ resource "aws_instance" "instance_1" {
 resource "aws_instance" "instance_2" {
   ami = var.ami
   instance_type = "t2.micro"
-  key_name = "and_project_instance_2"
+  key_name = "and_key_pair"
   associate_public_ip_address = true
   subnet_id = aws_subnet.public-subnet-2.id
   vpc_security_group_ids = ["${aws_security_group.app_security_group.id}"]
