517976b14071eccfd4b7f514a1cb05655c6d05fb
Omid Ayoobe <oayoo001@gold.ac.uk>
2021-03-08 14:08:19 +0000

    creating new key pairs

--- main.tf
+++ main.tf
@@ -27,3 +27,14 @@ resource "aws_instance" "instance_2" {
     Name = "instance_2"
   }
 }
+
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+}
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "and_key_pair"
+  public_key = tls_private_key.this.public_key_openssh
+}
