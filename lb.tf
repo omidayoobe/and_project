27b99eecb0ada8adb58ebcac43d15f8f9482aa8b
Omid Ayoobe <oayoo001@gold.ac.uk>
2021-03-12 15:23:43 +0000

    adding load balancer

--- lb.tf
+++ lb.tf
@@ -6,3 +6,13 @@
   }
       depends_on = [aws_subnet.public-subnet-1, aws_subnet.public-subnet-2]
 }
+
+
+  resource "aws_lb" "lb_and_project" {
+    name               = "and-load-balancer"
+    load_balancer_type = "application"
+    subnets            = data.aws_subnet_ids.and_project.ids
+    security_groups = [aws_security_group.app_security_group.id]
+
+    enable_cross_zone_load_balancing = true
+  } 
